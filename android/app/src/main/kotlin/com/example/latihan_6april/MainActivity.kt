48e85b8fe497003e7d4a326cae468f5916413c98
Karinaqurani <qurkarina580@gmail.com>
2024-06-29 22:42:50 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/latihan_6april/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.latihan_6april
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screen_page/detail_berita.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import '../model/ModelBerita.dart';
+
+
+
+
+class DetailBeritaPage extends StatelessWidget {
+  final Datum? berita;
+
+  const DetailBeritaPage(this.berita, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(berita?.judul ?? "Detail Berita"),
+      ),
+      body: SingleChildScrollView(         child: Column(
+        children: [
+          if (berita?.gambar != null)
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.network(
+                  'http://192.168.243.79/edukasi_server/gambar_berita/${berita!.gambar}',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              berita?.judul ?? "",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              berita?.berita ?? "",
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+          ),
+        ],
+      ),
+      ),
+    );
+  }
+}
